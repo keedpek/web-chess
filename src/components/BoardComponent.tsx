10bba6fcd2c6ce91399611f8087ac5c050decc7f
keedpek <zaharv20004@gmail.com>
2024-03-28 02:54:06 +0300

    highlighting done

--- src/components/BoardComponent.tsx
+++ src/components/BoardComponent.tsx
@@ -1,8 +1,58 @@
-import React from 'react';
+import React, { FC, useEffect, useState } from 'react';
+import { Board } from '../models/Board';
+import CellComponent from './CellComonent';
+import { Cell } from '../models/Cell';
 
-const BoardComponent = () => {
+
+interface BoardProps {
+  board: Board;
+  setBoard: (board: Board) => void;
+}
+
+const BoardComponent: FC<BoardProps> = ({board, setBoard}) => {
+  const [selectedCell, setSelectedCell] = useState<Cell | null>(null);
+
+  useEffect(() => {
+    highlightCells();
+  }, [selectedCell]);
+
+  function click(cell:Cell) {
+    if (selectedCell && selectedCell !== cell && selectedCell.figure?.canMove(cell)) {
+      console.log(selectedCell);
+      selectedCell.moveFigure(cell);
+      setSelectedCell(null);
+      console.log(selectedCell);
+    } else if (selectedCell === cell) {
+      setSelectedCell(null);
+    } else if (cell.figure) {
+      setSelectedCell(cell);
+    }
+  }
+
+  function highlightCells() {
+    board.highlightCells(selectedCell);
+    updateBoard();
+  }
+
+  function updateBoard() {
+    const newBoard = board.getCopyBoard();
+    setBoard(newBoard);
+  }
+
   return (
     <div className='board'>
+      {board.cells.map((row, index) => 
+        <React.Fragment key={index}>
+          {row.map(cell => 
+            <CellComponent
+              click={click} 
+              cell={cell}
+              key={cell.id}
+              selected={selectedCell?.x === cell.x && selectedCell?.y === cell.y}
+             />  
+          )}
+        </React.Fragment>
+      )}
     </div>
   );
 }
--- src/components/CellComonent.tsx
+++ src/components/CellComonent.tsx
@@ -1,9 +1,20 @@
-import React from 'react';
+import React, { FC } from 'react';
+import { Cell } from '../models/Cell';
+import { click } from '@testing-library/user-event/dist/click';
 
-const CellComponent = () => {
+interface CellProps {
+  cell: Cell;
+  selected: boolean;
+  click: (cell: Cell) => void;
+}
+
+const CellComponent: FC<CellProps> = ({cell, selected, click}) => {
   return (
-    <div className='cell'>
-      
+    <div 
+    className={['cell', cell.color, selected ? "selected" : '', (cell.available && !cell.figure) ? 'available' : '', cell.available && cell.figure ? 'under-attack' : ''].join(' ')}
+    onClick={() => {click(cell)}}
+    >
+      {cell.figure?.logo && <img src={cell.figure.logo} alt='' />}
     </div>
   );
 }
